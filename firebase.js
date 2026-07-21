fba57e679a844e152f5a665a931b6b7927a0d276
wattimenakarel07-cpu <wattimenakarel07@gmail.com>
2026-07-22 02:57:43 +0900

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,103 @@
+// ======================================================
+// FIREBASE CONFIG
+// Fuel Management System
+// ======================================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvP56jnAHqdAq2uPaVMnk_4wBmMt5_bJM",
+  authDomain: "fuel-project-e128c.firebaseapp.com",
+  projectId: "fuel-project-e128c",
+  storageBucket: "fuel-project-e128c.firebasestorage.app",
+  messagingSenderId: "702150234067",
+  appId: "1:702150234067:web:7ed98001051c15cec06926"
+};
+
+// ======================================================
+// INISIALISASI FIREBASE
+// ======================================================
+
+firebase.initializeApp(firebaseConfig);
+
+// ======================================================
+// FIRESTORE DATABASE
+// ======================================================
+
+const db = firebase.firestore();
+
+// ======================================================
+// FUNGSI AMBIL DATA KARYAWAN
+// ======================================================
+
+async function getKaryawan(id) {
+  try {
+    const doc = await db.collection("karyawan").doc(id).get();
+
+    if (doc.exists) {
+      return doc.data();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Gagal mengambil data:", error);
+    return null;
+  }
+}
+
+// ======================================================
+// FUNGSI TAMBAH KARYAWAN
+// ======================================================
+
+async function tambahKaryawan(id, data) {
+  try {
+    await db.collection("karyawan").doc(id).set(data);
+    alert("Data karyawan berhasil disimpan.");
+  } catch (error) {
+    console.error(error);
+    alert("Gagal menyimpan data.");
+  }
+}
+
+// ======================================================
+// FUNGSI UPDATE KARYAWAN
+// ======================================================
+
+async function updateKaryawan(id, data) {
+  try {
+    await db.collection("karyawan").doc(id).update(data);
+    alert("Data berhasil diperbarui.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// ======================================================
+// FUNGSI HAPUS KARYAWAN
+// ======================================================
+
+async function hapusKaryawan(id) {
+  try {
+    await db.collection("karyawan").doc(id).delete();
+    alert("Data berhasil dihapus.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// ==============================
+// TEST KONEKSI FIREBASE
+// ==============================
+
+db.collection("karyawan")
+  .doc("hfnc-kwp-2409059")
+  .get()
+  .then((doc) => {
+    if (doc.exists) {
+      console.log("Firebase Terhubung");
+      console.log(doc.data());
+    } else {
+      console.log("Data tidak ditemukan");
+    }
+  })
+  .catch((error) => {
+    console.error("Error Firebase:", error);
+  });
